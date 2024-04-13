92e7229fb19f9cea3bc4bee6987e1f24e303d4cb
Pawan <pawansharma93520@gmail.com>
2024-04-13 21:10:05 +0530

    some other exersice is complete

---
+++ ex12/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Exercise 1: Values, Variables, Constants
+
+	// var x int = 10
+	// const y float64 = 3.14
+	// fmt.Printf("x: %d, Type: %T\n", x, x)
+	// fmt.Printf("y: %f, Type: %T\n", y, y)
+
+	// // Exercise 2: For Loop
+
+	// fmt.Println("First 10 even numbers:")
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Println(2 * i)
+	// }
+
+	// Exercise 3: If/Else
+	// Write your code here
+
+	// var Userinput int
+	// fmt.Println("Enter Your number :")
+	// fmt.Scanf("%d", &Userinput)
+	// if Userinput < 0 {
+	// 	fmt.Println("Your integer is Nagtive")
+	// } else if Userinput > 0 {
+	// 	fmt.Println("Your integer is Positive")
+	// } else {
+	// 	fmt.Println("Your integer is Zero")
+
+	// }
+
+	// Exercise 4: Switch Statement
+	// Write your code here
+
+	var day string
+	fmt.Println("Give a Weekday :")
+	fmt.Scanf("%s", &day)
+	switch day {
+	case "Saturday", "Sunday","saturday", "sunday":
+		fmt.Println("Today Is Weekends")
+	default:
+		fmt.Println("Today is Working day")
+	}
+
+	// Exercise 5: Arrays
+	// Write your code here
+
+	// Exercise 6: Slices
+	// Write your code here
+}
